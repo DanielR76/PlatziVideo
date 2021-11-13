b59a1cca93664fbfb42f6a6d5cf801c84fa91dc2
Daniel <danielr2397@gmail.com>
2021-11-13 17:25:58 -0500

    Add redux & state

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import { BrowserRouter as Routes, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import Home from "../containers/Home";
 import Login from "../containers/Login";
 import Register from "../containers/Register";
 import NotFound from "../containers/NotFound";
 import Layout from "../components/Layout";
 
+import initialState from "../../initialState.json";
+import reducer from "../reducers";
+
+const store = createStore(reducer, initialState.initalState);
+
 const App = () => (
-  <Routes>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
-  </Routes>
+  <Provider store={store}>
+    <Routes>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </Routes>
+  </Provider>
 );
 
 export default App;
